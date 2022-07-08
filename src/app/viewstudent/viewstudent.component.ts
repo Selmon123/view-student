fce46ff912e5223f03b4c8c2b8a768492a1c86a3
Selmon123 <sudhil.sujith7384@gmail.com>
2022-07-08 11:46:10 +0530

    details

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -11,6 +11,13 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  name="Selmon"
+  data=[
+    {"name":"Selmon","age":50,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"},
+    {"name":"Shahrukh","age":60,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"},
+    {"name":"Saif","age":30,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"},
+    {"name":"Sohail","age":90,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"},
+    {"name":"Amitabh","age":70,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"},
+    {"name":"Parv","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg4h1B8CMfadXgfKeNcNleBEARIvHzdnjZRw&usqp=CAU"}
+  ]
 
 }
